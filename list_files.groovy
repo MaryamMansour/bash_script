a227db2d955e1cd2541b4b07432a932c0eac0331
Maryam Mansour <106898803+MaryamMansour@users.noreply.github.com>
2024-05-14 14:59:34 +0300

    Update list_files.groovy

--- list_files.groovy
+++ list_files.groovy
@@ -2,11 +2,7 @@ pipeline{
     agent any
 
     stages{
-        stage('Checkout'){
-            steps{
-                git url:'https://github.com/MaryamMansour/bash_script.git'
-            }
-        }
+     
         stage('Execute'){
             steps{
                 bat 'dir'
